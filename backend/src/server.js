bc83925bfc257154d3d58dea97c00ef5239ca47a
Nilesh <nileshswain715@gmail.com>
2026-02-06 18:41:05 +0530

    error solved-02

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,7 +18,7 @@ const app = express();
 // 3. Middleware
 // Added specific methods to avoid CORS issues during redirects
 app.use(cors({
-      origin: process.env.CLIENT_URL,
+      origin: true,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
