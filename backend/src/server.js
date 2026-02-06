08887d17aea19fe0166e779ecbce80af2b360cf7
Nilesh <nileshswain715@gmail.com>
2026-02-06 18:25:44 +0530

    pushed it

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,7 +18,7 @@ const app = express();
 // 3. Middleware
 // Added specific methods to avoid CORS issues during redirects
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api/auth', // <--- Check this carefully
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true
 });
 
 export default api;
